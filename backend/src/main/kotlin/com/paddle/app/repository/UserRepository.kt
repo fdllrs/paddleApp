64bd7cd91f59dfde8eed3a2972730e304f328d5c
nazolaz <Nazareno_Olaz@hotmail.com>
2026-03-11 00:40:47 -0300

    userRepository added

--- backend/src/main/kotlin/com/paddle/app/repository/UserRepository.kt
+++ backend/src/main/kotlin/com/paddle/app/repository/UserRepository.kt
@@ -2,8 +2,11 @@ package com.paddle.app.repository
 
 import com.paddle.app.model.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.UUID
 
 interface UserRepository: JpaRepository<User, UUID> {
-
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    fun findUserById(@Param("id") id: UUID): User?
 }
