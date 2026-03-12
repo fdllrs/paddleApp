72f82ebd84a476a2146510899b9032da33374ff4
nazolaz <Nazareno_Olaz@hotmail.com>
2026-03-11 22:40:45 -0300

    club in intersection query added

--- backend/src/main/kotlin/com/paddle/app/repository/ClubRepository.kt
+++ backend/src/main/kotlin/com/paddle/app/repository/ClubRepository.kt
@@ -1,8 +1,24 @@
 package com.paddle.app.repository
 
 import com.paddle.app.model.Club
+import org.locationtech.jts.geom.Point
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.UUID
 
-interface ClubRepository: JpaRepository<Club, UUID> {
+interface ClubRepository : JpaRepository<Club, UUID> {
+
+    @Query("""
+        SELECT (COUNT(c) > 0) 
+        FROM Club c 
+        WHERE ST_DWithin(c.location, :location1, :radius1) = true 
+        AND ST_DWithin(c.location, :location2, :radius2) = true
+    """)
+    fun existsClubInIntersection(
+        @Param("location1") location1: Point,
+        @Param("radius1") radius1: Double,
+        @Param("location2") location2: Point,
+        @Param("radius2") radius2: Double
+    ): Boolean
 }
